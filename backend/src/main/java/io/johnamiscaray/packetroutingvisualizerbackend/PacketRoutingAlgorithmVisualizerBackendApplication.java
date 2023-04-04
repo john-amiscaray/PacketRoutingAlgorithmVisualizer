52fb732ad38178e8261394bff57cfa53712d99c1
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-04-04 18:10:56 -0400

    Init backend

---
+++ backend/src/main/java/io/johnamiscaray/packetroutingvisualizerbackend/PacketRoutingAlgorithmVisualizerBackendApplication.java
@@ -0,0 +1,13 @@
+package io.johnamiscaray.packetroutingvisualizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketRoutingAlgorithmVisualizerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacketRoutingAlgorithmVisualizerBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/io/johnamiscaray/packetroutingvisualizerbackend/PacketRoutingAlgorithmVisualizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.johnamiscaray.packetroutingvisualizerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketRoutingAlgorithmVisualizerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
