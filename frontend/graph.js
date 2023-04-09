d366cfdb08aad550a13b247d73fb049ed813d71d
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-04-08 20:06:59 -0400

    Add graph updates

--- frontend/graph.js
+++ frontend/graph.js
@@ -5,4 +5,49 @@ let graph = {
 
 };
 
-export { graph };
+function addNode(label) {
+
+    if(graph.nodes.filter(node => node.label === label).length !== 0){
+        return {
+            success: false,
+            error: "A node with that label already exists"
+        };
+    }
+    graph.nodes.push({ label });
+    return {
+        success: true
+    };
+
+}
+
+function addEdge(node1, node2, weight){
+
+    if(graph.nodes.filter(node => node.label === node1).length === 0 || graph.nodes.filter(node => node.label === node2).length === 0){
+        return {
+            success: false,
+            error: "The new edge contains non-existent nodes"
+        };
+    }else if(node1 === node2){
+        return {
+            success: false,
+            error: "The graph cannot contain loops"
+        };
+    }
+    graph.edges.push({ node1, node2, weight });
+    return {
+        success: true
+    };
+
+}
+
+function removeNode(label) {
+
+    graph.nodes = graph.nodes.filter(node => node.label !== label);
+    graph.edges = graph.edges.filter(edge => edge.node1 !== label && edge.node2 !== label);
+    return {
+        success: true
+    };
+
+}
+
+export { graph, addNode, addEdge, removeNode };
--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,51 @@
-import { graph } from "./graph.js";
+import { addEdge, addNode, graph, removeNode } from "./graph.js";
 
 let stage = new createjs.Stage("graphics-pane");
+
+function editGraphSubmit() {
+
+    let newNodeLabel = $("#add-node").val().trim();
+    let newEdgeStart = $("#node-from").val().trim();
+    let newEdgeEnd = $("#node-to").val().trim();
+    let newEdgeWeight = $("#weight").val();
+    let nodeToDelete = $("#delete-node").val().trim();
+    let results = [];
+    let errorMessage = "Failed to apply changes for the following reasons:\n";
+    let failed = false;
+
+    if(newNodeLabel){
+
+        results.push(addNode(newNodeLabel));
+
+    }
+
+    if(nodeToDelete){
+
+        results.push(removeNode(nodeToDelete));
+
+    }
+
+    if(newEdgeStart && newEdgeEnd && newEdgeWeight){
+
+        results.push(addEdge(newEdgeStart, newEdgeEnd, newEdgeWeight));
+
+    }
+
+    $('input').val("");
+
+    for(let result of results){
+
+        if(result.success === false){
+            failed = true;
+            errorMessage += `${result.error}\n`;
+        }
+
+    }
+    if(failed){
+        alert(errorMessage);
+    }
+    console.log(graph);
+
+}
+
+window.editGraphSubmit = editGraphSubmit;
