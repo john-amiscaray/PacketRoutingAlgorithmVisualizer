e7d749854d8091d535c4d3c586f1327979f579e7
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-04-08 21:15:55 -0400

    Added computeDijkstra setup

--- frontend/graph.js
+++ frontend/graph.js
@@ -1,4 +1,4 @@
-let storedGraph = localStorage.getItem("graph");
+let storedGraph = localStorage.getItem('graph');
 
 let graph = storedGraph ? JSON.parse(storedGraph) : { nodes: [], edges: [] };
 
@@ -7,7 +7,7 @@ function addNode(label) {
     if(graph.nodes.filter(node => node.label === label).length !== 0){
         return {
             success: false,
-            error: "A node with that label already exists"
+            error: 'A node with that label already exists'
         };
     }
     graph.nodes.push({ label });
@@ -22,12 +22,12 @@ function addEdge(node1, node2, weight){
     if(graph.nodes.filter(node => node.label === node1).length === 0 || graph.nodes.filter(node => node.label === node2).length === 0){
         return {
             success: false,
-            error: "The new edge contains non-existent nodes"
+            error: 'The new edge contains non-existent nodes'
         };
     }else if(node1 === node2){
         return {
             success: false,
-            error: "The graph cannot contain loops"
+            error: 'The graph cannot contain loops'
         };
     }
     graph.edges.push({ node1, node2, weight });
@@ -49,8 +49,23 @@ function removeNode(label) {
 
 function saveGraph() {
 
-    localStorage.setItem("graph", JSON.stringify(graph));
+    localStorage.setItem('graph', JSON.stringify(graph));
 
 }
 
-export { graph, addNode, addEdge, removeNode, saveGraph };
+async function computeDijkstra(start) {
+
+    return await fetch('http://localhost:8080/graph/dijkstra', { 
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            graph,
+            start
+        })
+    });
+
+}
+
+export { graph, addNode, addEdge, removeNode, saveGraph, computeDijkstra };
