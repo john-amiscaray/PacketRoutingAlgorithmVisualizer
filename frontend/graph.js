7d403e08e1ed72a6c91825b07ce668c10757fe4c
Nahid <nahid02m@gmail.com>
2023-04-09 22:23:47 -0400

    Seperated update button into add node, add edge, delete node

--- frontend/graph.js
+++ frontend/graph.js
@@ -1,71 +1,66 @@
-let storedGraph = localStorage.getItem('graph');
+let storedGraph = localStorage.getItem("graph");
 
 let graph = storedGraph ? JSON.parse(storedGraph) : { nodes: [], edges: [] };
 
 function addNode(label) {
-
-    if(graph.nodes.filter(node => node.label === label).length !== 0){
+    if (graph.nodes.filter((node) => node.label === label).length !== 0) {
         return {
             success: false,
-            error: 'A node with that label already exists'
+            error: "A node with that label already exists",
         };
     }
     graph.nodes.push({ label });
     return {
-        success: true
+        success: true,
     };
-
 }
 
-function addEdge(node1, node2, weight){
-
-    if(graph.nodes.filter(node => node.label === node1).length === 0 || graph.nodes.filter(node => node.label === node2).length === 0){
+function addEdge(node1, node2, weight) {
+    if (
+        graph.nodes.filter((node) => node.label === node1).length === 0 ||
+        graph.nodes.filter((node) => node.label === node2).length === 0
+    ) {
         return {
             success: false,
-            error: 'The new edge contains non-existent nodes'
+            error: "The new edge contains non-existent nodes",
         };
-    }else if(node1 === node2){
+    } else if (node1 === node2) {
         return {
             success: false,
-            error: 'The graph cannot contain loops'
+            error: "The graph cannot contain loops",
         };
     }
     graph.edges.push({ node1, node2, weight });
     return {
-        success: true
+        success: true,
     };
-
 }
 
 function removeNode(label) {
-
-    graph.nodes = graph.nodes.filter(node => node.label !== label);
-    graph.edges = graph.edges.filter(edge => edge.node1 !== label && edge.node2 !== label);
+    graph.nodes = graph.nodes.filter((node) => node.label !== label);
+    graph.edges = graph.edges.filter(
+        (edge) => edge.node1 !== label && edge.node2 !== label
+    );
     return {
-        success: true
+        success: true,
     };
-
 }
 
 function saveGraph() {
-
-    localStorage.setItem('graph', JSON.stringify(graph));
-
+    localStorage.setItem("graph", JSON.stringify(graph));
 }
 
 async function computeDijkstra(start) {
-
-    return await fetch('http://localhost:8080/graph/dijkstra', { 
-        method: 'POST',
+    return await fetch("http://localhost:8080/graph/dijkstra", {
+        method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
             graph,
-            start
-        })
+            start,
+        }),
     });
-
 }
 
 export { graph, addNode, addEdge, removeNode, saveGraph, computeDijkstra };
